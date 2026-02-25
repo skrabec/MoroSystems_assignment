1141080ecba48917d5f108f83a9ad763e81de3a1
Nikolaj Skrabec <skrabets@gmail.com>
2026-02-25 21:17:04 +0100

    Created UI tests

--- src/main/java/extensions/GuicePageModule.java
+++ src/main/java/extensions/GuicePageModule.java
@@ -9,23 +9,46 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.Tracing;
+import com.microsoft.playwright.options.ViewportSize;
+import pages.GooglePage;
+import pages.KarieryPage;
+import pages.MoroSystemsPage;
 
 public class GuicePageModule extends AbstractModule {
     private Page page;
     private BrowserContext context;
+    public static final boolean TRACING_ENABLED = Boolean.parseBoolean(System.getProperty("tracing.enabled", "false"));
 
     public GuicePageModule() {
         Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(
-            new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
+        String browserName = System.getProperty("browser", "chromium");
+        BrowserType.LaunchOptions options = new BrowserType.LaunchOptions()
+            .setHeadless(false)
+            .setSlowMo(500);
 
-        BrowserContext context = browser.newContext();
+        Browser browser;
+        switch (browserName.toLowerCase()) {
+            case "firefox":
+                browser = playwright.firefox().launch(options);
+                break;
+            case "edge":
+                browser = playwright.chromium().launch(options.setChannel("msedge"));
+                break;
+            default:
+                browser = playwright.chromium().launch(options);
+                break;
+        }
 
-        context
-            .tracing()
-            .start(new Tracing.StartOptions()
-                .setScreenshots(false)
-                .setSnapshots(false));
+        BrowserContext context = browser.newContext(new Browser.NewContextOptions()
+            .setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+            .setLocale("cs-CZ")
+            .setViewportSize(new ViewportSize(1920, 1080)));
+
+        if (TRACING_ENABLED) {
+            context.tracing().start(new Tracing.StartOptions()
+                .setScreenshots(true)
+                .setSnapshots(true));
+        }
 
         this.page = context.newPage();
         this.context = context;
@@ -33,7 +56,32 @@ public class GuicePageModule extends AbstractModule {
 
     @Singleton
     @Provides
+    public Page getPage() {
+        return page;
+    }
+
+    @Singleton
+    @Provides
     public BrowserContext getContext() {
         return context;
     }
+
+    @Singleton
+    @Provides
+    public GooglePage getGooglePage() {
+        return new GooglePage(page);
+    }
+
+    @Singleton
+    @Provides
+    public MoroSystemsPage getMoroSystemsPage() {
+        return new MoroSystemsPage(page);
+    }
+
+    @Singleton
+    @Provides
+    public KarieryPage getKarieryPage() {
+        return new KarieryPage(page);
+    }
+
 }
--- src/main/java/extensions/junit/UIExtensions.java
+++ src/main/java/extensions/junit/UIExtensions.java
@@ -8,18 +8,13 @@ import extensions.GuicePageModule;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class UIExtensions implements BeforeEachCallback, AfterEachCallback {
     Injector injector;
-    String timestamp = generateTimestamp();
-
-    private static String generateTimestamp() {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
-        return sdf.format(new Date());
-    }
 
     @Override
     public void beforeEach(ExtensionContext context) throws Exception {
@@ -29,7 +24,12 @@ public class UIExtensions implements BeforeEachCallback, AfterEachCallback {
 
     @Override
     public void afterEach(ExtensionContext context) {
-        injector.getProvider(BrowserContext.class).get().tracing()
-            .stop(new Tracing.StopOptions().setPath(Paths.get(timestamp + "_trace.zip")));
+        if (GuicePageModule.TRACING_ENABLED) {
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String testName = context.getDisplayName().replaceAll("[^a-zA-Z0-9]", "_");
+            injector.getProvider(BrowserContext.class).get().tracing()
+                .stop(new Tracing.StopOptions()
+                    .setPath(Paths.get("target/" + testName + "_" + timestamp + "_trace.zip")));
+        }
     }
 }
